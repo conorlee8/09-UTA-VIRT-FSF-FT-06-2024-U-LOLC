62b513cb414fa6b87a4beed1003b548429977c18
Conor Lee <your emailconorlee8@gmail.com>
2024-08-01 11:08:45 -0500

    big update1

---
+++ index.js
@@ -0,0 +1,100 @@
+// We will import the required packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// This is where we define the questions to prompt the user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide the usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide the contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide the test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
+
+// Here is the function to generate README content based on user input
+function generateReadme(answers) {
+    return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact me with the information below:
+
+GitHub: [${answers.github}](https://github.com/${answers.github})  
+Email: ${answers.email}
+  `;
+}
+
+// We use Inquirer to prompt the user and then write the README file
+inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+});
