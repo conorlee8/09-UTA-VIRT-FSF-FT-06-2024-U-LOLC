76e5d146bf8be44be5b5f46cd365a6def8277aab
Conor Lee <your emailconorlee8@gmail.com>
2024-08-27 10:05:01 -0500

    forgot to add

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# 09-UTA-VIRT-FSF-FT-06-2024-U-LOLC
-This is a repository for the 09-UTA-VIRT-FSF-FT-06-2024-U-LOLC challenge.
+
+# Eve
+
+## Description
+She is great!
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Feed her before bedtime
+
+## Usage
+only once a day!
+
+## License
+This project is licensed under the Apache 2.0 license.
+
+## Contributing
+no water!
+
+## Tests
+or sunlight
+
+## Questions
+For any questions, please contact me with the information below:
+
+GitHub: [evelyons17](https://github.com/evelyons17)  
+Email: eve@lyons.com
+  
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-// We will import the required packages
+// Import the required packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// This is where we define the questions to prompt the user
+// Define the questions to prompt the user
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Provide the installation instructions:',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide the usage information:',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Provide the contribution guidelines:',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Provide the test instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your project:',
-        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:',
-    },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide the installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide the usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide the contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide the test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
 ];
 
-// Here is the function to generate README content based on user input
+// Function to generate README content based on user input
 function generateReadme(answers) {
-    return `
+  return `
 # ${answers.title}
 
 ## Description
@@ -91,10 +91,16 @@ Email: ${answers.email}
   `;
 }
 
-// We use Inquirer to prompt the user and then write the README file
+// Use Inquirer to prompt the user and then write the README file
 inquirer.prompt(questions).then((answers) => {
-    const readmeContent = generateReadme(answers);
-    fs.writeFile('README.md', readmeContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-    );
+  console.log('User Answers:', answers);  // Debug log
+  const readmeContent = generateReadme(answers);
+  console.log('Generated README Content:', readmeContent);  // Debug log
+  fs.writeFile('README.md', readmeContent, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('Successfully created README.md!');
+    }
+  });
 });
